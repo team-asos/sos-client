3f96afb5eefc8f56e989e5f97adb447ff70bb207
Sennie Lim <sennie@Sennies-MacBook-Air.local>
2021-11-05 13:32:12 +0900

    좌석 도면 추가

--- src/components/a5_Cell.js
+++ src/components/a5_Cell.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 const Cell = props => {
-  console.log(props.data);
-  let renderCell = () => {
-    if (props.data.isOpen) {
-      return <div className="cell open">h</div>;
-    } else {
-      return (
-        <div className="cell" onClick={() => console.log('Clicked!')}>
-          hh
-        </div>
-      );
-    }
-  };
-  return renderCell();
+  return (
+    <div
+      className="cell"
+      onClick={() => props.clickCell(props.data)}
+      //onClick={() => console.log(props.data)}
+      style={{ pointer: 'cursor' }}
+    ></div>
+  );
 };
 
 export default Cell;
