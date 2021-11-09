c9a0051a324f07629101486fc912eef4c2f2a4fe
jiwon111 <dreamjiwon@naver.com>
2021-11-10 02:34:59 +0900

    토큰 수정

--- src/components/u1_roomInfoTable.js
+++ src/components/u1_roomInfoTable.js
@@ -18,7 +18,7 @@ const RoomInfoTable = () => {
       await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/rooms`, {
         headers: {
           Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcsIm5hbWUiOiLquYDsubTtgqQiLCJyb2xlIjowLCJpYXQiOjE2MzYzODQ5OTcsImV4cCI6MTYzNjQ3MTM5N30.U9xQrCi51sBaempL6yQa3boHV8ZiO0si0OHD-vkqDK4',
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6Iuq5gOyngOybkCIsInJvbGUiOjAsImlhdCI6MTYzNjQ3NzQ3MSwiZXhwIjoxNjM2NTYzODcxfQ.NJJnkUwA2_y45ZMR5o3GVDh6TlUCkBsMn5hdWsh6pyw',
         },
         method: 'GET',
       })
@@ -29,9 +29,10 @@ const RoomInfoTable = () => {
     };
     res();
   }, []);
+  console.log(data);
   return (
     <div>
-      <Table className="infoTable">
+      <Table striped hover className="infoTable">
         <thead className="rHeader">
           <tr>
             <th>회의실 명</th>
