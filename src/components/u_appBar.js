eaf1c088c55b0eadcc256266f4d8d5473b939bbc
jiwon111 <dreamjiwon@naver.com>
2021-11-12 01:46:58 +0900

    스타일 수정

--- src/components/u_appBar.js
+++ src/components/u_appBar.js
@@ -4,7 +4,6 @@ import AccountIcon from './icons/u_accountIcon';
 import SeatReservationIcon from './icons/u_seatReservationIcon';
 import RoomReservationIcon from './icons/u_roomReservationIcon';
 import InquireIcon from './icons/u_inquireIcon';
-import SettingIcon from './icons/u_settingIcon';
 
 class AppBox extends React.Component {
   render() {
