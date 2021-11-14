e9ff8d19a42263977e93a725189b8fb0347d391d
jiwon111 <dreamjiwon@naver.com>
2021-11-14 23:54:41 +0900

    회원 id 추가

--- src/pages/u5_seatPage.js
+++ src/pages/u5_seatPage.js
@@ -1,56 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NavBarUser from '../components/u_navBar';
 import SeatStatusForm from '../components/u5_seatStatusForm';
 import UserSearchForm from '../components/u5_userSearchForm';
 import DateTimeForm from '../components/u5_dateTimeForm';
+import { useCookies } from 'react-cookie';
 
 import '../assets/styles/u5_seatPage.css';
 //좌석 예약 페이지
-class SeatPage extends React.Component {
-  render() {
-    return (
-      <div className="userSeatPage">
-        <div>
-          <NavBarUser />
+const SeatPage = () => {
+  const [myId, setMyId] = useState();
+  const [cookie] = useCookies(['access_token']);
+
+  /*내 정보 */
+  useEffect(() => {
+    const res = async () => {
+      await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/auth`, {
+        headers: {
+          Authorization: `Bearer ${cookie.access_token}`,
+        },
+        method: 'GET',
+      })
+        .then(response => response.json())
+        .then(json => {
+          setMyId(json.id);
+        });
+    };
+    res();
+  }, []);
+  return (
+    <div className="userSeatPage">
+      <div>
+        <NavBarUser />
+      </div>
+
+      <div className="userSeatForm">
+        <div className="u_seatHeader">
+          <div className="u_seatHeaderTextStyle">
+            <Link
+              to="/seat-reservation"
+              style={{
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              좌석 예약
+            </Link>
+          </div>
         </div>
 
-        <div className="userSeatForm">
-          <div className="u_seatHeader">
-            <div className="u_seatHeaderTextStyle">
-              <Link
-                to="/seat-reservation"
-                style={{
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                좌석 예약
-              </Link>
-            </div>
+        <div className="userSeatReservationForm">
+          {/*좌석 도면, 시설, 좌석 현황*/}
+          <div className="u_leftForm">
+            <SeatStatusForm />
           </div>
 
-          <div className="userSeatReservationForm">
-            {/*좌석 도면, 시설, 좌석 현황*/}
-            <div className="u_leftForm">
-              <SeatStatusForm />
-            </div>
+          {/*임직원 검색창, 예약시간, 예약하기 버튼*/}
+          <div className="u_rightForm">
+            <DateTimeForm myId={myId} />
+            <UserSearchForm />
 
-            {/*임직원 검색창, 예약시간, 예약하기 버튼*/}
-            <div className="u_rightForm">
-              <DateTimeForm />
-              <UserSearchForm />
-
-              {/*<button
+            {/*<button
                 className="u_seatReservationButton"
               >
                 예약하기
               </button>*/}
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default SeatPage;
