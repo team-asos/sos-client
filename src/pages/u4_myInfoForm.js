d671ccd0ccc7c1cbf5ddc109410f48702869c3b6
jiwon111 <dreamjiwon@naver.com>
2021-11-16 00:58:11 +0900

    비밀번호 변경 추가

--- src/pages/u4_myInfoForm.js
+++ src/pages/u4_myInfoForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
-
+import DeleteAccount from '../components/u4_deleteAccount';
+import MyReservationList from '../components/u4_myReservationListForm';
 import NavBarUser from '../components/u_navBar';
 import { Link } from 'react-router-dom';
 import '../assets/styles/u4_myInfoForm.css';
@@ -8,7 +9,10 @@ import '../assets/styles/u4_myInfoForm.css';
 const MyInfoForm = props => {
   const [cookie] = useCookies(['access_token']);
   const [myData, setMyData] = useState([]);
+  const [state, setState] = useState(0);
   const [disable, setDisable] = useState(1);
+  const [user, setUser] = useState({});
+  console.log(props.match.params.idx);
   const [editData, setEditData] = useState({
     name: '',
     email: '',
@@ -16,6 +20,8 @@ const MyInfoForm = props => {
     tel: '',
     department: '',
     position: '',
+    password: '',
+    confirmPw: '',
   });
   useEffect(() => {
     const res = async () => {
@@ -42,6 +48,22 @@ const MyInfoForm = props => {
           });
         });
     };
+
+    res();
+  }, []);
+  useEffect(() => {
+    const res = async () => {
+      await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/auth`, {
+        headers: {
+          Authorization: `Bearer ${cookie.access_token}`,
+        },
+        method: 'GET',
+      })
+        .then(response => response.json())
+        .then(json => {
+          setUser(json);
+        });
+    };
     res();
   }, []);
   /*정보 수정 */
@@ -61,28 +83,13 @@ const MyInfoForm = props => {
           tel: editData.tel,
           department: editData.department,
           position: editData.position,
+          password: editData.password,
         }),
       },
     );
     alert(response.status);
   };
-  /*탈퇴 */
-  const dropHandler = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_SERVER_BASE_URL}/users/${props.match.params.idx}`,
-      {
-        headers: {
-          'Content-type': 'application/json',
-          Authorization: `Bearer ${cookie.access_token}`,
-        },
-        method: 'DELETE',
-      },
-    );
-    if (response.status === 200) {
-      alert('탈퇴가 완료되었습니다.');
-      //removeCookie('access_token');
-    }
-  };
+
   const editHandler = () => {
     setDisable(!disable);
   };
@@ -124,6 +131,29 @@ const MyInfoForm = props => {
       position: e.target.value,
     });
   };
+  const editPw = e => {
+    setEditData({
+      ...editData,
+      password: e.target.value,
+    });
+  };
+  const editConfirmPw = e => {
+    setEditData({
+      ...editData,
+      confirmPw: e.target.value,
+    });
+  };
+  const confirmPwHandler = () => {
+    if (editData.password !== editData.confirmPw) {
+      setDisable(1); //다름
+      alert('비밀번호가 다릅니다.');
+    } else {
+      setDisable(0);
+    }
+  };
+  const clickHandler = id => {
+    setState(id);
+  };
   return (
     <div className="userMyPage">
       <div>
@@ -143,141 +173,145 @@ const MyInfoForm = props => {
             </Link>
           </div>
 
-          <div className="myRLMenuTextStyle">
-            <Link
-              to="/user-mypage"
-              style={{
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              나의 예약 내역
-            </Link>
+          <div onClick={() => clickHandler(2)} className="myRLMenuTextStyle">
+            나의 예약 내역
           </div>
-          <div className="myInfoMenuTextStyle">나의 정보 수정</div>
+          <div onClick={() => clickHandler(0)} className="myInfoMenuTextStyle">
+            나의 정보 수정
+          </div>
+          <div onClick={() => clickHandler(1)} className="myInfoMenuTextStyle">
+            회원 탈퇴
+          </div>
         </div>
         <div className="myInfoForm">
-          <p className="myInfoFormTitleTextStyle">나의 정보 수정</p>
+          {state === 2 ? (
+            <MyReservationList user={user} />
+          ) : state === 1 ? (
+            <DeleteAccount user={user} />
+          ) : (
+            <>
+              <p className="myInfoFormTitleTextStyle">나의 정보 수정</p>
 
-          <div className="myInfoDetailForm">
-            <div className="column">
-              <div>
-                <label>이름</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.name}
-                  disabled={disable}
-                  onChange={editName}
-                  value={myData.name}
-                />
-              </div>
-              <div className="setPadding">
-                <label>이메일</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.email}
-                  disabled={disable}
-                  onChange={editEmail}
-                  value={myData.email}
-                />
-              </div>
-            </div>
+              <div className="myInfoDetailForm">
+                <div className="column">
+                  <div>
+                    <label>이름</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.name}
+                      disabled={disable}
+                      onChange={editName}
+                      value={myData.name}
+                    />
+                  </div>
+                  <div className="setPadding">
+                    <label>이메일</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.email}
+                      disabled={disable}
+                      onChange={editEmail}
+                      value={myData.email}
+                    />
+                  </div>
+                </div>
 
-            <div className="column">
-              <div>
-                <label>비밀번호</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="대소문자와 숫자를 포함한 8~12자리"
-                  disabled={disable}
+                <div className="column">
+                  <div>
+                    <label>비밀번호</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="대소문자와 숫자를 포함한 8~12자리"
+                      disabled={disable}
+                      onChange={editPw}
+                      //value={password}
+                    />
+                  </div>
+                  <div className="setPadding">
+                    <label>비밀번호 확인</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      placeholder="비밀번호 확인"
+                      disabled={disable}
+                      onBlur={confirmPwHandler}
+                      onChange={editConfirmPw}
+                      //value={confirmPw}
+                    />
+                  </div>
+                </div>
 
-                  //onChange={inputPw}
-                  //value={password}
-                />
-              </div>
-              <div className="setPadding">
-                <label>비밀번호 확인</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="비밀번호 확인"
-                  disabled={disable}
+                <div className="column">
+                  <div>
+                    <label>사원번호</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.employeeId}
+                      disabled={disable}
+                      onChange={editEmployeeId}
+                      value={myData.employeeId}
+                    />
+                  </div>
+                  <div className="setPadding">
+                    <label>전화번호</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.tel}
+                      disabled={disable}
+                      onChange={editTel}
+                      value={myData.tel}
+                    />
+                  </div>
+                </div>
 
-                  // onBlur={confirmHandler}
-                  //onChange={inputConfirmPw}
-                  //value={confirmPw}
-                />
+                <div className="column">
+                  <div>
+                    <label>부서</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.department}
+                      disabled={disable}
+                      onChange={editDepartment}
+                      value={myData.department}
+                    />
+                  </div>
+                  <div className="setPadding">
+                    <label>직급</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder={myData.position}
+                      disabled={disable}
+                      onChange={editPosition}
+                      value={myData.position}
+                    />
+                  </div>
+                </div>
+                <div className="infoBottom">
+                  <button
+                    className="infoEditButton"
+                    onClick={editHandler}
+                    disabled={!disable}
+                  >
+                    수정
+                  </button>
+                  <button
+                    className="editFinishButton"
+                    disabled={disable}
+                    onClick={confirmHandler}
+                  >
+                    완료
+                  </button>
+                </div>
               </div>
-            </div>
-
-            <div className="column">
-              <div>
-                <label>사원번호</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.employeeId}
-                  disabled={disable}
-                  onChange={editEmployeeId}
-                  value={myData.employeeId}
-                />
-              </div>
-              <div className="setPadding">
-                <label>전화번호</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.tel}
-                  disabled={disable}
-                  onChange={editTel}
-                  value={myData.tel}
-                />
-              </div>
-            </div>
-
-            <div className="column">
-              <div>
-                <label>부서</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.department}
-                  disabled={disable}
-                  onChange={editDepartment}
-                  value={myData.department}
-                />
-              </div>
-              <div className="setPadding">
-                <label>직급</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder={myData.position}
-                  disabled={disable}
-                  onChange={editPosition}
-                  value={myData.position}
-                />
-              </div>
-            </div>
-            <div style={{ position: 'inherit' }}>
-              <button className="infoEditButton" onClick={editHandler}>
-                수정
-              </button>
-              <button
-                className="editFinishButton"
-                disabled={disable}
-                onClick={confirmHandler}
-              >
-                완료
-              </button>
-            </div>
-            <p className="dropUser" onClick={dropHandler}>
-              탈퇴
-            </p>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </div>
