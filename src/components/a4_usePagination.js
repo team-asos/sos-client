7c159688171846394e981d77d149ea725d7b6be0
seeun-lim <ssen.lim@gmail.com>
2021-11-16 14:29:16 +0900

    무엇인가 바뀜

--- src/components/a4_usePagination.js
+++ src/components/a4_usePagination.js
@@ -12,7 +12,10 @@ const UsePagination = ({ totalUsers, usersPerPage, paginate }) => {
       <ul className="pagination">
         {pageNumbers.map(number => (
           <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="#" className="page-link">
+            <a 
+            onClick={() => paginate(number)} 
+            href="#" 
+            className="page-link">
               {number}
             </a>
           </li>
