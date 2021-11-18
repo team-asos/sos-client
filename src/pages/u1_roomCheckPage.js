d6b130f9500458ff3fd39b007155bef54f3aaea4
jiwon111 <dreamjiwon@naver.com>
2021-11-19 03:06:08 +0900

    반응형 추가

--- src/pages/u1_roomCheckPage.js
+++ src/pages/u1_roomCheckPage.js
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBarUser from '../components/u_navBar';
 import RoomInfoTable from '../components/u1_roomInfoTable';
 import { Link } from 'react-router-dom';
 import '../assets/styles/u1_roomCheckPage.css';
-
+import { useMediaQuery } from 'react-responsive';
+import { FiMenu, FiX } from 'react-icons/fi';
 //회의실 조회 페이지
-class RoomCheckPage extends React.Component {
-  render() {
-    return (
-      <div className="roomCheckPage">
-        <div>
-          <NavBarUser />
+const RoomCheckPage = () => {
+  const [open, setOpen] = useState(false);
+  const isPc = useMediaQuery({
+    query: '(min-width:768px)',
+  });
+  const isMobile = useMediaQuery({ query: '(max-width:767px)' });
+  const navClick = () => {
+    console.log('clicked');
+    setOpen(1);
+    return <NavBarUser />;
+  };
+  return (
+    <div className="roomCheckPage">
+      <div>{isPc ? <NavBarUser /> : ''}</div>
+
+      <div className="roomCheckForm">
+        <div className="checkHeader">
+          <div>
+            {isMobile ? <FiMenu size={40} onClick={() => navClick} /> : ''}
+          </div>
+          <div className="roomCheck_titleTextStyle">
+            <Link
+              to="/room-check"
+              style={{
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              회의실 조회
+            </Link>
+          </div>
         </div>
 
-        <div className="roomCheckForm">
-          <div className="checkHeader">
-            <div className="roomCheck_titleTextStyle">
-              <Link
-                to="/room-check"
-                style={{
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                회의실 조회
-              </Link>
-            </div>
-          </div>
-
-          <div className="roomInfoTable">
-            <RoomInfoTable />
-          </div>
+        <div className="roomInfoTable">
+          <RoomInfoTable />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default RoomCheckPage;
