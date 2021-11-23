0d446a101bfe01253432d775db189a9e1cf687fb
jiwon111 <dreamjiwon@naver.com>
2021-11-24 02:16:12 +0900

    그냥

--- src/components/u2_roomTimeTable.js
+++ src/components/u2_roomTimeTable.js
@@ -5,48 +5,165 @@ import getMonth from 'date-fns/getMonth';
 import { useCookies } from 'react-cookie';
 import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 import Calendar from './u2_calendar';
+import { Table } from 'react-bootstrap';
 import { useMediaQuery } from 'react-responsive';
 
 import '../assets/styles/u2_roomTimeTable.css';
-//회의실 예약 페이지-> 선택된 회의실 설명 테이블(room-check에서 정보 받아오기)
-const RoomTimeTable = selectedDate => {
-  console.log(selectedDate);
-  const clickHandler = () => {
-    console.log('클릭');
+const RoomTimeTable = ({ selectedDate, roomId }) => {
+  const [timeTable, setTimeTable] = useState([]);
+  const [name, setName] = useState('Mark');
+  const [isClicked, setIsClicked] = useState(0);
+  const [firstClick, setFirstClick] = useState(0);
+  const [secondClick, setSecondClick] = useState(0);
+  //const [selectedTimeColor, setSelectedTimeColor] = useState('white');
+
+  const clickHandler = e => {
+    if (!isClicked) {
+      setFirstClick(e);
+    } else if (isClicked) {
+      setSecondClick(e);
+    }
+    setIsClicked(!isClicked);
+    console.log(firstClick, secondClick);
   };
+  useEffect(() => {
+    const fetchRoomTable = async () => {
+      const result = await fetch(
+        `${process.env.REACT_APP_SERVER_BASE_URL}/reservations/room/${roomId}/table`,
+        {
+          headers: {
+            //Authorization: `Bearer ${cookie.access_token}`,
+            'Content-type': 'application/json',
+            Accept: 'application/json',
+          },
+          method: 'GET',
+        },
+      )
+        .then(response => response.json())
+        .then(json => {
+          setTimeTable(json);
+        });
+    };
+    fetchRoomTable();
+  }, []);
   return (
     <div
       style={{
-        marginTop: '2%',
+        marginTop: '1%',
         width: '60%',
         height: '70vh',
-        backgroundColor: 'yellow',
+        overflow: 'auto',
       }}
     >
-      <MDBTable>
-        <MDBTableHead>
+      <Table>
+        <thead>
           <tr>
             <th style={{ width: '20%' }}>시간</th>
             <th>참석자</th>
           </tr>
-        </MDBTableHead>
-        <MDBTableBody>
+        </thead>
+        <tbody>
           <tr>
-            <td>1</td>
-            <td onClick={() => clickHandler()} style={{ cursor: 'pointer' }}>
+            <td key={1} className={isClicked ? 'clickTime' : 'time'}>
+              8:00
+            </td>
+            <td onClick={() => clickHandler(1)} style={{ cursor: 'pointer' }}>
               Mark
             </td>
           </tr>
           <tr>
-            <td>2</td>
+            <td
+              key={2}
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(2)}
+            >
+              8:30
+            </td>
             <td>Jacob</td>
           </tr>
           <tr>
-            <td>3</td>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(3)}
+            >
+              9:00
+            </td>
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(4)}
+            >
+              9:30
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(5)}
+            >
+              10:00
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(6)}
+            >
+              10:30
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(7)}
+            >
+              11:00
+            </td>
+            <td></td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(8)}
+            >
+              11:30
+            </td>
             <td>Larry</td>
           </tr>
-        </MDBTableBody>
-      </MDBTable>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(9)}
+            >
+              12:00
+            </td>
+            <td>Larry</td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(10)}
+            >
+              12:30
+            </td>
+            <td>Larry</td>
+          </tr>
+          <tr>
+            <td
+              className={isClicked ? 'clickTime' : 'time'}
+              onClick={() => clickHandler(11)}
+            >
+              13:00
+            </td>
+            <td>Larry</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 };
