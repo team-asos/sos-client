4811d6e7e2948117aba5e1024ec9c9b26a87ff91
jiwon111 <dreamjiwon@naver.com>
2021-11-24 02:14:50 +0900

    검색한 직원의 위치 추가

--- src/components/u5_userSearchForm.js
+++ src/components/u5_userSearchForm.js
@@ -5,30 +5,81 @@ import { Table } from 'react-bootstrap';
 import '../assets/styles/u5_userSearchForm.css';
 //좌석 예약 페이지->직원 검색
 const UserSearchForm = () => {
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
   const [ID, setID] = useState();
   const [cookie] = useCookies(['access_token']);
-
+  const [userReservation, setUserReservation] = useState([]);
+  const [userLocation, setUserLocation] = useState({
+    floor: '', //층 이름
+    location: '', //좌석이름/회의실이름
+    status: '', //사용중/미팅중
+    floorId: 0,
+    seatId: 0,
+  });
+  /*검색한 직원의 ID */
   const handleChange = value => {
     setID(value);
   };
-
   useEffect(() => {
-    const res = async () => {
+    const fetchUsers = async () => {
       await fetch(`${process.env.REACT_APP_SERVER_BASE_URL}/users/search`, {
         headers: {
           Authorization: `Bearer ${cookie.access_token}`,
+          'Content-type': 'application/json',
         },
         method: 'GET',
       })
         .then(response => response.json())
         .then(json => {
-          setData(json);
+          setUsers(json);
         });
     };
-    res();
+    fetchUsers();
   }, []);
-
+  const fetchUserReservation = async () => {
+    await fetch(
+      `${process.env.REACT_APP_SERVER_BASE_URL}/reservations/search?userId=${ID}`,
+      {
+        headers: {
+          'Content-type': 'application/json',
+        },
+        method: 'GET',
+      },
+    )
+      .then(response => response.json())
+      .then(json => {
+        setUserReservation(json);
+        json.map(item => {
+          if (item.status === 1 && item.room == null) {
+            setUserLocation({
+              floor: item.seat.floor.name,
+              location: item.seat.name,
+              status: '사용중',
+              floorId: item.seat.floor.id,
+              seatId: item.seat.id,
+            });
+          }
+          if (item.status === 1 && item.room != null) {
+            setUserLocation({
+              floor: item.room.floor.name,
+              location: item.room.name,
+              status: '미팅중',
+            });
+          } else if (item.status === 0 || item.status === 2) {
+            setUserLocation({
+              floor: '', //층 이름
+              location: '',
+              status: '퇴근',
+            });
+          }
+        });
+      });
+  };
+  useEffect(() => {
+    if (ID !== undefined) {
+      fetchUserReservation();
+    }
+  }, [ID]);
   return (
     <div className="u_userSearchForm">
       <div className="searchUserTextStyle">
@@ -37,7 +88,7 @@ const UserSearchForm = () => {
       <div>
         <Select
           menuPosition={'center'}
-          options={data.map(item => ({
+          options={users.map(item => ({
             value: item.id,
             label: [item.department + '  ' + item.name + '  ' + item.position],
           }))}
@@ -49,7 +100,7 @@ const UserSearchForm = () => {
         />
       </div>
       <div className="userLocation">
-        {data.map(item =>
+        {users.map(item =>
           item.id === ID ? (
             <>
               <Table striped hover className="userLocationInfo">
@@ -82,16 +133,14 @@ const UserSearchForm = () => {
                 <tbody>
                   <tr>
                     <td />
-                    <td>1층</td>
-                    <td>좌석/회의실 이름</td>
-                    <td>사용중/미팅중/퇴실?</td>
+                    <td>{userLocation.floor}</td>
+                    <td>{userLocation.location}</td>
+                    <td>{userLocation.status}</td>
                   </tr>
                 </tbody>
               </Table>
             </>
-          ) : (
-            ''
-          ),
+          ) : null,
         )}
       </div>
     </div>
