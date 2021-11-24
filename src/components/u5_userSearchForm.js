91958b24c526ec91def4411a299267dd7bcd410a
jiwon111 <dreamjiwon@naver.com>
2021-11-24 14:53:17 +0900

    회의실 테이블 추가

--- src/components/u5_userSearchForm.js
+++ src/components/u5_userSearchForm.js
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { useCookies } from 'react-cookie';
 import { Table } from 'react-bootstrap';
+import { formatISO } from 'date-fns';
+
 import '../assets/styles/u5_userSearchForm.css';
 //좌석 예약 페이지->직원 검색
 const UserSearchForm = () => {
+  const now = formatISO(new Date());
   const [users, setUsers] = useState([]);
   const [ID, setID] = useState();
   const [cookie] = useCookies(['access_token']);
   const [userReservation, setUserReservation] = useState([]);
   const [userLocation, setUserLocation] = useState({
-    floor: '', //층 이름
-    location: '', //좌석이름/회의실이름
-    status: '', //사용중/미팅중
-    floorId: 0,
-    seatId: 0,
+    seatFloor: '',
+    roomFloor: '',
+    seatLocation: '',
+    roomLocation: '',
+    seatStatus: '',
+    roomStatus: '',
   });
   /*검색한 직원의 ID */
   const handleChange = value => {
@@ -49,37 +53,43 @@ const UserSearchForm = () => {
       .then(response => response.json())
       .then(json => {
         setUserReservation(json);
-        json.map(item => {
-          if (item.status === 1 && item.room == null) {
-            setUserLocation({
-              floor: item.seat.floor.name,
-              location: item.seat.name,
-              status: '사용중',
-              floorId: item.seat.floor.id,
-              seatId: item.seat.id,
-            });
-          }
-          if (item.status === 1 && item.room != null) {
-            setUserLocation({
-              floor: item.room.floor.name,
-              location: item.room.name,
-              status: '미팅중',
-            });
-          } else if (item.status === 0 || item.status === 2) {
-            setUserLocation({
-              floor: '', //층 이름
-              location: '',
-              status: '퇴근',
-            });
-          }
-        });
       });
+  };
+  const search = () => {
+    console.log('들어옴');
+    console.log(userReservation);
+    userReservation.map(item => {
+      if (item.room === null && item.status === 1) {
+        if (item.endTime === null) {
+          setUserLocation({
+            seatFloor: item.seat.floor.name,
+            seatLocation: item.seat.name,
+            seatStatus: '사용중',
+          });
+        }
+      }
+      if (item.status === 1 && item.room != null) {
+        if (item.endTime >= now) {
+          console.log('미팅중', item.id);
+          setUserLocation({
+            roomFloor: item.room.floor.name,
+            roomLocation: item.room.name,
+            roomStatus: '미팅중',
+          });
+        }
+      }
+    });
   };
   useEffect(() => {
     if (ID !== undefined) {
       fetchUserReservation();
     }
   }, [ID]);
+  useEffect(() => {
+    if (userReservation !== undefined) {
+      search();
+    }
+  }, [userReservation]);
   return (
     <div className="u_userSearchForm">
       <div className="searchUserTextStyle">
@@ -126,16 +136,34 @@ const UserSearchForm = () => {
                   <tr>
                     <th></th>
                     <th>층</th>
-                    <th>위치</th>
-                    <th>상태</th>
+                    <th>좌석 위치</th>
+                    <th>이용 상태</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
                     <td />
-                    <td>{userLocation.floor}</td>
-                    <td>{userLocation.location}</td>
-                    <td>{userLocation.status}</td>
+                    <td>{userLocation.seatFloor}</td>
+                    <td>{userLocation.seatLocation}</td>
+                    <td>{userLocation.seatStatus}</td>
+                  </tr>
+                </tbody>
+              </Table>
+              <Table striped hover className="userLocationInfo">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>층</th>
+                    <th>회의실 위치</th>
+                    <th>이용 상태</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td />
+                    <td>{userLocation.roomFloor}</td>
+                    <td>{userLocation.roomLocation}</td>
+                    <td>{userLocation.roomStatus}</td>
                   </tr>
                 </tbody>
               </Table>
