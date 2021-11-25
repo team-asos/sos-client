f310438a85864fee577103518b3ed356ce9802cc
jiwon111 <dreamjiwon@naver.com>
2021-11-26 01:56:14 +0900

    회의실 예약

--- src/components/u2_roomTimeTable.js
+++ src/components/u2_roomTimeTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import { useCookies } from 'react-cookie';
 import { Table } from 'react-bootstrap';
 import { formatISO } from 'date-fns';
@@ -10,13 +10,23 @@ import '../assets/styles/u2_roomTimeTable.css';
 const RoomTimeTable = ({ MAXUSER, selectedDate, roomId }) => {
   const [timeTable, setTimeTable] = useState([]);
   const [isReserved, setIsReserved] = useState([]);
+  //const isClicked = Array.from({ length: 20 }, () => 0); 이렇게 하고 싶지만 1로 바꿔주는게 밑에 있어서 색상 적용이 안됨
   const [start, setStart] = useState('');
   const [end, setEnd] = useState('');
-  const clickHandler = (startTime, endTime) => {
-    console.log(startTime, endTime);
-    setStart(startTime);
-    setEnd(endTime);
-  };
+  const [isClicked, setIsClicked] = useState([]);
+
+  const [firstClick, setFirstClick] = useState({
+    Start: '',
+    End: '',
+    isClicked: 0,
+    idx: null,
+  });
+  const [secondClick, setSecondClick] = useState({
+    Start: '',
+    End: '',
+    isClicked: 0,
+    idx: null,
+  });
   const fetchRoomTable = async () => {
     const result = await fetch(
       `${
@@ -36,6 +46,79 @@ const RoomTimeTable = ({ MAXUSER, selectedDate, roomId }) => {
       .then(json => {
         setTimeTable(json);
       });
+  };
+  const tableColorStyle = isClicked => {
+    if (isClicked) return { backgroundColor: 'crimson' };
+    else if (!isClicked) return { backgroundColor: 'none' };
+  };
+  const clickHandler = (startTime, endTime, i, e) => {
+    if (!firstClick.isClicked) {
+      //첫번째가 선택 안됐을 때
+      setFirstClick({
+        Start: startTime,
+        End: endTime,
+        isClicked: 1,
+        idx: i,
+      });
+      //firstClick.startTime으로 바꾸고 싶은데 비동기 처리를 못 하겠음
+      //일단 임시방편임 실시간으로 input값에 들어가게 하고 싶어서
+      setStart(startTime);
+      setEnd(endTime);
+    } else if (firstClick.isClicked) {
+      //여기는 firstClick에 값 들어감
+      //첫번째가 선택 됐을 때
+      if (startTime === firstClick.Start && endTime === firstClick.End) {
+        //똑같은거 다시 눌렀을 때
+        setFirstClick({ Start: '', End: '', isClicked: 0, idx: null }); //초기화
+        setStart('');
+        setEnd('');
+      } else if (!secondClick.isClicked) {
+        //첫번째랑 다를 때 두번째로 설정
+        setSecondClick({
+          Start: startTime,
+          End: endTime,
+          isClicked: 1,
+          idx: i,
+        });
+      } else if (secondClick.isClicked) {
+        //누르자 마자 바뀌어야하기 때문에 여기에 있는게 맞음
+        for (let i = firstClick.idx; i < secondClick.idx; i++) {
+          isClicked[i] = 0;
+        }
+        setFirstClick({ Start: '', End: '', isClicked: 0, idx: null }); //초기화
+        setSecondClick({ Start: '', End: '', isClicked: 0, idx: null }); //초기화
+        //setStart('');//초기화 하면 input값이 사라짐 ㅠ
+        setEnd('');
+      }
+    }
+    //여기부터는 first,second뜸
+    //setSecondeClick 설정되고 바로 설정해주고 싶음
+    if (secondClick.idx) {
+      if (secondClick.idx > firstClick.idx) {
+        setEnd(secondClick.End); //이것도 위에서 해주면 좋음
+        for (let i = 0; i <= timeTable.length; i++) {
+          //시작~끝 색상 변경하기 위해.. 원래는 위에서 secondClick되자마자 해줘야함
+          if (i >= firstClick.idx && i <= secondClick.idx) {
+            isClicked[i] = 1;
+          } else {
+            isClicked[i] = 0;
+          }
+        }
+      } else {
+        alert('시작 시간을 먼저 선택해주세요.');
+        setStart('');
+        setEnd('');
+      }
+    }
+  };
+  const deleteSelection = () => {
+    for (let i = 0; i < 20; i++) {
+      isClicked[i] = 0;
+    }
+    setFirstClick({ Start: '', End: '', isClicked: 0, idx: null }); //초기화
+    setSecondClick({ Start: '', End: '', isClicked: 0, idx: null }); //초기화
+    setStart('');
+    setEnd('');
   };
   useEffect(() => {
     if (roomId !== null && selectedDate !== null) {
@@ -71,7 +154,9 @@ const RoomTimeTable = ({ MAXUSER, selectedDate, roomId }) => {
         <Table>
           <thead>
             <tr>
-              <th style={{ width: '20%' }}>시간</th>
+              <th style={{ width: '20%' }}>
+                시간<button onClick={() => deleteSelection()}>선택 취소</button>
+              </th>
               <th style={{ width: '10%' }}>대표자</th>
               <th>참석자</th>
             </tr>
@@ -80,34 +165,31 @@ const RoomTimeTable = ({ MAXUSER, selectedDate, roomId }) => {
             {timeTable &&
               timeTable.map((item, idx) => (
                 <tr key={idx}>
+                  {/*시간 */}
                   <td
                     className={isReserved[idx] ? 'isReserved' : 'isNotReserved'}
                     onClick={
                       !isReserved[idx]
-                        ? () => clickHandler(item.start_time, item.end_time)
+                        ? e =>
+                            clickHandler(item.start_time, item.end_time, idx, e)
                         : null
                     }
+                    style={tableColorStyle(isClicked[idx])}
                   >
                     {item.start_time}-{item.end_time}
                   </td>
                   {item.id ? (
-                    <td
-                      className={
-                        isReserved[idx] ? 'isReserved' : 'isNotReserved'
-                      }
-                    >
-                      {item.user.name}
-                    </td>
+                    <td className="isReserved">{item.user.name}</td>
                   ) : (
                     <td></td>
                   )}
                   {item.id ? (
-                    <td
-                      className={
-                        isReserved[idx] ? 'isReserved' : 'isNotReserved'
-                      }
-                    >
-                      {item.participants.map(users => users.user.name)}
+                    <td className="isReserved">
+                      {item.participants.map((users, idx) =>
+                        idx === item.participants.length - 1
+                          ? users.user.name
+                          : users.user.name + ', ',
+                      )}
                     </td>
                   ) : (
                     <td></td>
@@ -122,6 +204,7 @@ const RoomTimeTable = ({ MAXUSER, selectedDate, roomId }) => {
         END={end}
         MAXUSER={MAXUSER}
         ROOMID={roomId}
+        selectedDate={selectedDate}
       />
     </>
   );
